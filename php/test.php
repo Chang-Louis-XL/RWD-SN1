a2267ab87d5b06d71011256a2ca2bacf85562dbf
Louis Chang <louisvictory0226@gmail.com>
2024-04-25 16:25:31 +0800

    121

--- php/test.php
+++ php/test.php
@@ -15,23 +15,15 @@
 </head>
 <body>
 <?php
-// 赋值: <body text='black'>
-$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
 
-// 赋值: Hll Wrld f PHP
-$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
-$onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
+$birthday="1990-2-26";
+$start=strtotime(date("y-m-d"));
 
-// 赋值: You should eat pizza, beer, and ice cream every day
-$phrase  = "You should eat fruits, vegetables, and fiber every day.";
-$healthy = array("fruits", "vegetables", "fiber");
-$yummy   = array("pizza", "beer", "ice cream");
 
-$newphrase = str_replace($healthy, $yummy, $phrase);
 
-// 赋值: 2
-$str = str_replace("ll", "", "good golly miss molly!", $count);
-echo $count;
+echo $start
+
+
 ?>
 
 </body>
