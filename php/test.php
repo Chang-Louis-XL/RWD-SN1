39413494e225ccc1e905bc5920de61b46bc11a37
Louis Chang <louisvictory0226@gmail.com>
2024-04-29 16:37:32 +0800

    121

--- php/test.php
+++ php/test.php
@@ -13,106 +13,53 @@
 </head>
 
 <body>
-<h2>線上月曆製作</h2>
-    <ul>
-        <li>以表格方式呈現整個月份的日期</li>
-        <li>可以在特殊日期中顯示資訊(假日或紀念日)</li>
-        <li>嘗試以block box或flex box的方式製作月曆</li>
-    </ul>
+
+    <h2>建立一個學生成績陣列</h2>
+
 
     <?php
-    $month = 5;
-    echo "月份:" . $month;
-    echo "<br>";
-    // ("Y-$month-1"):-1顯示每月的第一天
-    $firstDay = strtotime(date("Y-$month-1"));
-    //date括弧逗點的後面放判斷依據的值，前為呈現結果類型的值
-    // ($firstweekstartday-1):確認每個月1號是禮拜幾
-    $firstWeekStartDay = date("w", $firstDay);
-    echo "第一周的開始是星期" . $firstWeekStartDay ;
-    // 查看該月有幾天
-    $days = date("t", $firstDay);
-    $lastDay = strtotime(date("Y-$month-$days"));
-    echo "<br>";
-    echo "最後一天是" . date("Y-m-d", $lastDay);
-    /* 
-    4  1  1 -0
-    5  3  3 -2
-    6  6  6 -5
-    7  1  1 -0
-    */
-    echo "<table>";
-    echo "<tr>";
-    echo "<td>日</td>";
-    echo "<td>一</td>";
-    echo "<td>二</td>";
-    echo "<td>三</td>";
-    echo "<td>四</td>";
-    echo "<td>五</td>";
-    echo "<td>六</td>";
-    echo "</tr>";
-    for ($i = 0; $i < 6; $i++) {
-        echo "<tr>";
-        for ($j = 0; $j < 7; $j++) {
-            if ($i == 0 && $j >= $firstWeekStartDay) {
-                echo "<td>";
-                // ($firstweekstartday-1):確認每個月1號是禮拜幾
-                // 若不是禮拜日，在1號前的變為空格
-                echo $i * 7 + $j - ($firstWeekStartDay - 1);
-                echo "</td>";
-            } else if ($i > 0) {
-                echo "<td>";
-                if ($i * 7 + $j - ($firstWeekStartDay - 1) <= $days) {
-                    echo $i * 7 + $j - ($firstWeekStartDay - 1);
-                } else {
-                    echo "&nbsp;";
-                }
-                echo "</td>";
+    $a = ['judy', 'amo', 'john', 'peter', 'hebe'];
 
-            } else {
-                echo "<td></td>";
-            }
+    $b = [
+        '國文' => [95, 88, 45, 59, 71],
+        '英文' => [64, 78, 60, 32, 62],
+        '數學' => [70, 54, 68, 77, 80],
+        '地理' => [90, 81, 70, 54, 62],
+        '歷史' => [84, 71, 62, 42, 64],
+    ];
+
+    for ($i = 0; $i < 5; $i++) {
+        for($j=0 ; $j<5;$j++){
+        echo  $a[$i];  
         }
-        echo "</tr>";
+        
+           
+
     }
 
 
+    echo "<table border='1'>";
 
-    echo "<table>";
+    // 輸出標題行
+    echo "<tr>";
+    echo "<th>名字</th>"; // 顯示名字標題
+    foreach (array_keys($b) as $subject) {
+        echo "<th>$subject</th>"; // 顯示科目標題
+    }
+    echo "</tr>";
+    
+    // 輸出成績
+    for ($i = 0; $i < 5; $i++) {
         echo "<tr>";
-        echo "<td>日</td>";
-        echo "<td>一</td>";
-        echo "<td>二</td>";
-        echo "<td>三</td>";
-        echo "<td>四</td>";
-        echo "<td>五</td>";
-        echo "<td>六</td>";
-        echo "</tr>";
-        for($i=0;$i<6;$i++){  
-            echo "<tr>";
-            for($j=0;$j<7;$j++){
-                if($i==0 && $j>=$firstWeekStartDay ){
-                    echo "<td>";
-                    echo    $i*7+$j-($firstWeekStartDay-1)  ."<br>﹝i列".$i.",j欄".$j."﹞";
-                    echo "<br>".$i."*7+".$j."-(".$firstWeekStartDay."-1)";
-                    echo "</td>";
-                }else if($i>0){
-                    echo "<td>";
-                    if($i*7+$j-($firstWeekStartDay-1)<=$days){
-                        echo $i*7+$j-($firstWeekStartDay-1) ."<br>﹝i列".$i.',j欄'.$j."﹞";
-                        echo "<br>".$i."*7+".$j."-(".$firstWeekStartDay."-1)";
-                    }else{
-                        echo "&nbsp;";
-                    }
-                    echo "</td>";
-                    
-                }else{
-                    echo "<td></td>";
-                }
-            }
-            echo "</tr>";
+        echo "<td>{$a[$i]}</td>"; // 顯示名字
+        foreach ($b as $subjectScores) {
+            echo "<td>{$subjectScores[$i]}</td>"; // 顯示每個科目的分數
         }
-        echo "</table>";
+        echo "</tr>";
+    }
+    
+    // 使用 HTML 表格結束
+    echo "</table>";
 
 
     ?>
