9b6fc1be9bea15ad76404f131cd45c7ad8c076bb
Louis Chang <louisvictory0226@gmail.com>
2024-09-02 09:41:17 +0800

    re

---
+++ a20240801_weather_api/js/weather.js
@@ -0,0 +1,2 @@
+let mykey="CWA-E7D86F2F-8688-4660-9446-876F7D985AEA"
+let weatherUrl= "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-E7D86F2F-8688-4660-9446-876F7D985AEA"
